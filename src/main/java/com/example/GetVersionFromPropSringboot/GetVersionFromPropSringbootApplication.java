4d5c0c0407672c30cbc908699109cd7dd17add03
Tejas7875 <tejas@inexture.com>
2023-10-27 12:31:20 +0530

    commit1

---
+++ src/main/java/com/example/GetVersionFromPropSringboot/GetVersionFromPropSringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.GetVersionFromPropSringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetVersionFromPropSringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetVersionFromPropSringbootApplication.class, args);
+		System.out.println("Testing/..");
+	}
+
+}
---
+++ src/test/java/com/example/GetVersionFromPropSringboot/GetVersionFromPropSringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GetVersionFromPropSringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetVersionFromPropSringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
